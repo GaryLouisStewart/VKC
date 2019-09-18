e7c9c5191a88f830f2a49adda6b967458ebafb13
Gary Louis Stewart <stewartlgary@gmail.com>
2019-09-19 00:52:24 +0100

    Make gen_certs bash script respectable

--- certs/scripts/gen_certs.sh
+++ certs/scripts/gen_certs.sh
@@ -1,29 +1,35 @@
 #!/bin/sh
 # generate vault certificates for TLS communication
 
+set -eo pipefail
+
 echo "Getting golang packages cfssl & cfssljson........."
 go get -u github.com/cloudflare/cfssl/cmd/cfssl
 go get -u github.com/cloudflare/cfssl/cmd/cfssljson
 
 echo "Generating a certificate autority......."
-cfssl gencert -initca ../config/ca-csr.json | cfssljson -bare ../../certs/ca
+[[ -d ../../certs/ca ]] || mkdir -p ../../certs/ca
+cfssl gencert -initca ../config/ca-csr.json | cfssljson -bare ../../certs/ca/ca
 
 echo "Creating private key and TLS certificates for Consul......"
+[[ -d ../consul ]] || mkdir -p ../consul
 
 cfssl gencert \
-    -ca=../../certs/ca.pem \
-    -ca-key=../../certs/ca-key.pem \
+    -ca=../../certs/ca/ca.pem \
+    -ca-key=../../certs/ca/ca-key.pem \
     -config=../config/ca-config.json \
     -profile=default \
-    ../config/consul-csr.json | cfssljson -bare ../../certs/consul
+    ../config/consul-csr.json | cfssljson -bare ../../certs/consul/consul
 
 echo "Generating Private key and TLS certificates for Vault......"
+
+[[ -d ../vault ]] || mkdir -p ../vault
 
 mkdir -p vault
 
 cfssl gencert \
-    -ca=../../certs/ca.pem \
-    -ca-key=../../certs/ca-key.pem \
+    -ca=../../certs/ca/ca.pem \
+    -ca-key=../../certs/ca/ca-key.pem \
     -config=../../certs/config/ca-config.json \
     -profile=default \
-    ../../certs/config/vault-csr.json | cfssljson -bare ../../certs/vault
+    ../../certs/config/vault-csr.json | cfssljson -bare ../../certs/vault/vault
